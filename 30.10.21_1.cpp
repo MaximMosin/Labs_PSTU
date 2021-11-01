4289e55415243851ec04d88f173fd70197da49cf
Maxim Mosin <92542116+MaximMosin@users.noreply.github.com>
2021-11-02 02:03:41 +0500

    Update 30.10.21_1.cpp

--- 30.10.21_1.cpp
+++ 30.10.21_1.cpp
@@ -1,64 +1,101 @@
 #include <iostream>
-#include <string>
-#include <stdlib.h>
-#include <cstdlib>
-#include <sstream>
 using namespace std;
 
-int main (void)
+int main()
 {
 	setlocale (LC_ALL, "Russian");
-	int x=0; //введенное число
-	string str; //введенное число типа int преобразованная в строку типа string
-	string a,b,c; //разбитое число на цифры
-	string x1,x2,x3,x4,x5,x6; //комбинации из цифр
-	int q,w,e,r,t,y,max; //комбинации типа string преобразованные в тип int (для нахождения максимального числа)
-	
-	while ((x<100)||(x>999)) //проверка на трехзначное число
-	{
-		cout << "Введите любую комбинацию из трех цифр" << endl;
-		cin >> x;
+	int x; //число, которое ввел пользователь
+	int a,b,c; //число разбитое на цифры
+	int e;
+	bool flag = false;
+
+	cout << "Введите такое трехзначное число, чтобы ни одна цифра в нем не повторялась:" << endl;
+	cin >> x;
+
+	while (flag == false){
+		a = x / 100; //первая цифра
+		b = (x / 10) % 10; //вторая цифра
+		c = x % 10; //третья цифра
+		if (x > 100) { //проверка чиисла на трехзначность
+			if (x < 999){
+				if (a != b){ //проверка числа на неповторяющиеся цифры
+					if (b != c){
+						if (c != a){
+							flag = true;
+						}
+					}
+				}
+			}
+		}
+		if (flag == false) {
+			cout << "Введенное число не соответствует требованиям." << endl << "Введите новое трехзначное число, где ни одна цифра не повторяется:" << endl;
+			cin >> x;
+		}
 	}
+	cout << "Все возможные комбинации:" << endl;
+	cout << a << b << c << endl; 
+	swap (b,c);
+	cout << a << b << c << endl; 
+	swap (a,c);
+	cout << a << b << c << endl; 
+	swap (b,c);
+	cout << a << b << c << endl; 
+	swap (a,c);
+	cout << a << b << c << endl; 
+	swap (b,c);
+	cout << a << b << c << endl;
 
-	ostringstream stream; //преобразование введенного числа из типа int в тип string для отделения каждой цифры в отдельную переменную 
-	stream << x;
-	str = stream.str();
-	a = str;
-	b = str;
-	c = str;
+	if (a>b){
+		if (a>c){
+			if (b>c){
+			cout << "Максимальное: " << a << b << c << endl;
+			return 0;
+			}
+			else {
+				cout << "Максимальное: " << a << c << b << endl;
+				return 0;
+			}
+		}
+		else {
+			cout << "Максимальное: " << c << a << b << endl;
+			return 0;
+		}
+	}
+	if (b>a){
+		if (b>c){
+			if (a>c){
+			cout << "Максимальное: " << b << a << c << endl;
+			return 0;
+			}
+			else {
+				cout << "Максимальное: " << b << c << a << endl;
+				return 0;
+			}
+		}
+		else {
+			cout << "Максимальное: " << c << b << a << endl;
+			return 0;
+		}
+	}
+	if (c>a){
+		if (c>b){
+			if (b>a){
+				cout << "Максимальное: " << c << b << a << endl;
+				return 0;
+			}
+			else {
+				cout << "Максимальное: " << c << a << b << endl;
+				return 0;
+			}
+		}
+		else {
+			cout << "Максимальное: " << b << c << a << endl;
+			return 0;
+		}
+	}
+	
 
-	a.pop_back(); //удаляет две последние цифры для отделения первой цифры от остальных
-	a.pop_back();
-	b.pop_back();
-	b = string (b.rbegin(), b.rend()); //удаляет первую и последнюю цифру для отделения средней цифры от остальных
-	b.pop_back();
-	c = string (c.rbegin(), c.rend()); //удаляет две первые цифры для отделения третьей цифры от остальных
-	c.pop_back();
-	c.pop_back();
-
-	x1 = a+b+c; //комбинации
-	x2 = a+c+b;
-	x3 = b+a+c;
-	x4 = b+c+a;
-	x5 = c+a+b;
-	x6 = c+b+a;
-
-	q = atoi(x1.c_str()); //преобращование комбинаций из типа string в тип int для нахождения максимума
-	w = atoi(x2.c_str());
-	e = atoi(x3.c_str());
-	r = atoi(x4.c_str());
-	t = atoi(x5.c_str());
-	y = atoi(x6.c_str());
-
-	max = q; //нахождение максимума
-	if (w>max) max = w;
-	if (e>max) max = e;
-	if (r>max) max = r;
-	if (t>max) max = t;
-	if (y>max) max = y;
 
 
-	cout << "Комбинации:" << endl << q << endl << w << endl << e << endl << r << endl << t << endl << y << endl;
-	cout << "Максимальное: " << max;
 
 }
